4c462b25b499a7c3ded72e6a0531bcdadbf6bd95
Ronaldo Lipata <lipata.ronaldo@gmail.com>
2024-01-06 11:22:11 +0800

    feat: display balance in the interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
+import { useEffect, useState } from 'react';
 import './App.css';
-// import { backend } from './declarations/backend';
+import { backend } from './declarations/backend';
 
 function App() {
+  const [balance, setBalance] = useState<number | undefined>();
+  const [loading, setLoading] = useState<boolean>(false);
+
+  async function checkBalance() {
+    try {
+      setLoading(true);
+      const data = await backend.checkBalance();
+      setBalance(data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(function () {
+    checkBalance();
+  }, []);
+
   return (
     <div className="App">
       <h1>DBANK - A Decentralized Bank</h1>
+      <span>{loading ? 'loading' : balance}</span>
     </div>
   );
 }
