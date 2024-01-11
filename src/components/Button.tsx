d94ef940d294302abcfe6aae6ef6a7126a7d2210
Ronaldo Lipata <lipata.ronaldo@gmail.com>
2024-01-11 20:26:45 +0800

    feat: create button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,34 @@
+type ButtonProps = {
+  variant: string;
+  disabled: boolean;
+  onClick?: () => void;
+  children: React.ReactNode;
+};
+
+export default function Button({
+  variant,
+  disabled,
+  onClick,
+  children,
+}: ButtonProps) {
+  function getVariantClass() {
+    switch (variant) {
+      case 'primary':
+        return 'primary';
+      case 'secondary':
+        return 'secondary';
+      default:
+        return 'default';
+    }
+  }
+
+  return (
+    <button
+      className={`button ${getVariantClass()}`}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
